1f93cf346804884c372fa4329a37ef75a6e36580
JiaHui Zou <JohHui@outlook.com>
2019-11-29 22:42:54 +0800

    Add project files.

---
+++ JsonTypeDemo/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+
+namespace JsonTypeDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //[1]
+            Console.WriteLine("新数据源之json文件解析");
+
+            IConfiguration configuration =
+                new ConfigurationBuilder().SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("appSettings.json")
+                .Build();
+
+            Console.WriteLine($"LoginId is {configuration["LoginId"].ToString()}");
+            Console.WriteLine($"server is {configuration["DbConn:server"].ToString()}");
+            Console.WriteLine($"uid is {configuration["DbConn:uid"].ToString()}");
+
+            Console.ReadKey();
+        }
+    }
+}
